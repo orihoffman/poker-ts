139b0b38422e3fadf61ce9b7c638f397fdbf5b2e
orihoffmann <ori@bitvore.com>
2024-11-02 22:34:36 -0700

    add types

---
+++ dist/lib/types.d.ts
@@ -0,0 +1,6 @@
+export * from 'types/blinds';
+export * from 'types/chips';
+export * from 'types/forced-bets';
+export * from 'types/hole-cards';
+export * from 'types/seat-array';
+export * from 'types/seat-index';
--- src/types/types.ts
+++
@@ -1,6 +0,0 @@
-export * from './blinds';
-export * from './chips';
-export * from './forced-bets';
-export * from './hole-cards';
-export * from './seat-array';
-export * from './seat-index';
